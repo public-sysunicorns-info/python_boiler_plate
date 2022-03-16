597cd7926cc62053a5244275736ebfe21317e6f7
MirageCentury <victorien.vanroye@gmail.com>
2022-03-16 15:54:06 +0100

    chore: fix default execution context
    host=0.0.0.0, port=8080

--- src/main.py
+++ src/main.py
@@ -10,7 +10,9 @@ logger = logging.getLogger(__package__)
 if __name__ == "__main__":
     try:
         _run = uvicorn.run(
-            app="main:application"
+            app="main:application",
+            host="0.0.0.0",
+            port=8080
         )
     except Exception as e:
         logger.critical(msg="Application Crash on uvicorn.run")
