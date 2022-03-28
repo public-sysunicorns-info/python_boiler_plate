82d71721d4fc5487c87586ba2ac4e8fc98c25992
MirageCentury <victorien.vanroye@gmail.com>
2022-03-28 14:45:12 +0200

    refactor: put application_config as Singleton to prevent duplicate and put it imutable

--- src/application/container.py
+++ src/application/container.py
@@ -3,7 +3,7 @@ Packaging the container class
 """
 
 from dependency_injector.containers import DeclarativeContainer
-from dependency_injector.providers import Factory, Provider
+from dependency_injector.providers import Singleton, Provider
 
 from .config import ApplicationConfig, application_config_factory
 
@@ -12,4 +12,4 @@ class Container(DeclarativeContainer):
     Application container to register element for the dependency-injector
     """
     application_config: Provider[ApplicationConfig] = \
-        Factory(provides=application_config_factory)
+        Singleton(provides=application_config_factory)
